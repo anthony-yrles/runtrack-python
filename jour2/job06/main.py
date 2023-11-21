5c864f49bcd191c5d700ba917de36ffcf0c54dd1
Sluchai40 <mabbolos8@gmail.com>
2023-11-21 15:46:38 +0100

    Fin de jour 2

--- jour2/job06/main.py
+++ jour2/job06/main.py
@@ -1,10 +1,32 @@
+# Version 1
+
 for n in range(2, 1001):
     prime = True
     for i in range (2, n):
-        if n % i not in [1, n] == 0:
+        if n % i == 0:
             prime = False
     if prime == True:
         print(n)
+
+# Version évolué par cyril
+    
+for n in range(2, 1001):
+    prime = True
+    for i in range (2, n):
+        if not (n % i) :
+            prime = False
+            break
+    if prime == True:
+        print(n)
+        
+# Version de Kevin plus simple
+
+for n in range (2, 1001) :
+    for i in range (2, n) :
+        if n % i == 0:
+            break
+        else:
+            print(n)
         
 # Version évoluer max
 
--- jour2/plusLoin/main.py
+++ jour2/plusLoin/main.py
@@ -3,18 +3,21 @@ b = int(input("Taille de b: "))
 c = int(input("Taille de c: "))
 
 if a <= 0 or b <= 0 or c <= 0:
-    print("Triangle impossible mettre des valeurs supérieur ou égal a 0")
+    print("Merci de mettre des valeurs supérieur ou égal à 0")
+    a = int(input("Taille de a: "))
+    b = int(input("Taille de b: "))
+    c = int(input("Taille de c: "))
 else:
-    ("Il s'agit bien d'un triangle")
+    print("Il s'agit bien d'un triangle")
 
 if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
     print("Il s'agit d'un triangle rectangle")
         
-if a == b or a == c or c == b:
+if a == b != c or a == c !=b or c == b != a:
     print("Il s'agit d'un triangle isocèle")
 
-if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2 and a == b or a == c or c == b:
-    print("Il s'agit d'un triangle rectangle isocèle")
+if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2 and a == b != c or a == c != b or c == b != a:
+    print("Il s'agit d'un triangle rectangle et isocèle")
     
 if a == b and a == c:
     print("Il s'agit d'un triangle equilatéral")
