33a87b9dc2c60ef17865a372ad6c6f33dfde95ea
Sluchai40 <mabbolos8@gmail.com>
2023-11-21 12:19:31 +0100

    bientot l heude de manger

---
+++ jour2/job07/main.py
@@ -0,0 +1,8 @@
+chaine = "abcdefghijklmnopqrstuvwxyz" * 10
+
+pyramide = ""
+for i in range(0, len(chaine)):
+    pyramide += chaine[i]
+    if i % 2 == 0:
+        pyra = pyramide
+        print(pyra)
