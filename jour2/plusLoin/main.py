db598b2e554eff032e2277f3b306e23fca520797
Sluchai40 <mabbolos8@gmail.com>
2023-11-22 09:45:01 +0100

    Begin of the third day

--- jour2/plusLoin/main.py
+++ jour2/plusLoin/main.py
@@ -7,17 +7,16 @@ if a <= 0 or b <= 0 or c <= 0:
     a = int(input("Taille de a: "))
     b = int(input("Taille de b: "))
     c = int(input("Taille de c: "))
-else:
+
+if a + b > c and a + c > b and b + c > a:
+    if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
+        print("Il s'agit d'un triangle rectangle")
+    if a == b != c or a == c !=b or c == b != a:
+        print("Il s'agit d'un triangle isocèle")
+    if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2 and a == b != c or a == c != b or c == b != a:
+        print("Il s'agit d'un triangle rectangle et isocèle")
+    if a == b and a == c:
+        print("Il s'agit d'un triangle equilatéral")
     print("Il s'agit bien d'un triangle")
-
-if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2:
-    print("Il s'agit d'un triangle rectangle")
-        
-if a == b != c or a == c !=b or c == b != a:
-    print("Il s'agit d'un triangle isocèle")
-
-if a**2 + b**2 == c**2 or a**2 + c**2 == b**2 or b**2 + c**2 == a**2 and a == b != c or a == c != b or c == b != a:
-    print("Il s'agit d'un triangle rectangle et isocèle")
-    
-if a == b and a == c:
-    print("Il s'agit d'un triangle equilatéral")
+else:
+    print("Les valeurs données ne permettent pas de former un triangle")
